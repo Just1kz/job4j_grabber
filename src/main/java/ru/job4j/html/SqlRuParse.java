3d6b0c39596d5ba07dbc641008db8d276caf846d
Just1lz <exelent.pride@rambler.ru>
2021-01-15 02:41:27 +0700

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#409270]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -9,35 +9,13 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 public class SqlRuParse {
     public static void main(String[] args) throws Exception {
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements row = doc.select(".postslisttopic");
-        Elements row2 = doc.select(".altCol");
-        for (Element td : row) {
-            Element href = td.child(0);
-            System.out.println(href.attr("href"));
-            System.out.println(href.text());
-        }
         List<String> list = new ArrayList<>();
         List<String> input = new ArrayList<>();
         List<LocalDateTime> output = new ArrayList<>();
-        for (Element td : row2) {
-            list.add(td.text());
-        }
-        for (int x = 1; x < list.size(); x = x + 2) {
-                input.add(list.get(x));
-        }
-
-        for (String o : input) {
-            System.out.println(o);
-        }
-
         int year;
         String month;
         int day;
@@ -46,35 +24,46 @@ public class SqlRuParse {
         LocalDateTime rsl;
         LocalDate localDate = LocalDate.now();
         LocalTime localTime;
-        for (String x : input) {
-            String[] z = x.split(", ");
-            String[] time = z[1].split(":");
-            hours = Integer.parseInt(time[0]);
-            minutes = Integer.parseInt(time[1]);
-            localTime = LocalTime.of(hours, minutes);
-            if (z[0].length() < 3) {
-                String[] date = z[0].split(" ");
-                year = 2000 + Integer.parseInt(date[2]);
-                month = date[1];
-                day = Integer.parseInt(date[0]);
-                rsl = LocalDateTime.of(year, Objects.requireNonNull(createMonth(month)), day, hours, minutes);
-            } else {
-                String p = z[0];
-                switch (p.toLowerCase()) {
-                    case("сегодня"):
-                        localDate = LocalDate.now();
-                    case ("вчера"):
-                        localDate = LocalDate.now().minusDays(1);
-                    case ("позавчера"):
-                        localDate = LocalDate.now().minusDays(2);
-                    default:
-                        break;
-                }
-                rsl = LocalDateTime.of(localDate, localTime);
-            }
-            output.add(rsl);
-        }
 
+        for (int i = 1; i <= 5; i++) {
+            Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers/" + i).get();
+            Elements row = doc.select(".postslisttopic");
+            Elements row2 = doc.select(".altCol");
+            for (Element td : row) {
+                Element href = td.child(0);
+                System.out.println(href.attr("href"));
+                System.out.println(href.text());
+            }
+            for (Element td : row2) {
+                list.add(td.text());
+            }
+            for (int x = 1; x < list.size(); x = x + 2) {
+                input.add(list.get(x));
+            }
+            for (String x : input) {
+                String[] z = x.split(", ");
+                String[] time = z[1].split(":");
+                hours = Integer.parseInt(time[0]);
+                minutes = Integer.parseInt(time[1]);
+                localTime = LocalTime.of(hours, minutes);
+                String[] date = z[0].split(" ");
+                if (date.length == 3) {
+                    year = 2000 + Integer.parseInt(date[2]);
+                    month = date[1];
+                    day = Integer.parseInt(date[0]);
+                    rsl = LocalDateTime.of(year, Objects.requireNonNull(createMonth(month)), day, hours, minutes);
+                } else {
+                    if (z[0].equals("сегодня")) {
+                        localDate = LocalDate.now();
+                    }
+                    if (z[0].equals("вчера")) {
+                        localDate = LocalDate.now().minusDays(1);
+                    }
+                    rsl = LocalDateTime.of(localDate, localTime);
+                }
+                output.add(rsl);
+            }
+        }
         for (LocalDateTime zx : output) {
             System.out.println(zx);
         }
