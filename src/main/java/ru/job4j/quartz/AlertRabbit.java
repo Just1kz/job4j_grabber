e0b0d93add9a78e7e9ce1865905f357a921d53d5
Just1lz <exelent.pride@rambler.ru>
2021-01-14 18:01:10 +0700

    1.1. Job c параметрами [#409268]

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -5,6 +5,9 @@ import org.quartz.impl.StdSchedulerFactory;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.sql.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,30 +15,65 @@ import static org.quartz.JobBuilder.newJob;
 import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
 import static org.quartz.TriggerBuilder.newTrigger;
 
-public class AlertRabbit {
+public class AlertRabbit implements AutoCloseable {
+    private final int interval;
+    private final String url;
+    private final String login;
+    private final String password;
+
+    private Connection connection;
     private final Map<String, String> values = new HashMap<String, String>();
 
-    public static void main(String[] args) {
-        AlertRabbit alertRabbit = new AlertRabbit();
-        int x = alertRabbit.getSettingsFileProperties();
-        try {
-            Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
-            scheduler.start();
-            JobDetail job = newJob(Rabbit.class).build();
-            SimpleScheduleBuilder times = simpleSchedule()
-                    .withIntervalInSeconds(x)
-                    .repeatForever();
-            Trigger trigger = newTrigger()
-                    .startNow()
-                    .withSchedule(times)
-                    .build();
-            scheduler.scheduleJob(job, trigger);
-        } catch (SchedulerException se) {
-            se.printStackTrace();
+    public AlertRabbit() {
+        getSettingsFileProperties();
+        this.interval = Integer.parseInt(values.get("rabbit.interval"));
+        this.url = values.get("url");
+        this.login = values.get("login");
+        this.password = values.get("password");
+        if (url == null
+                || login == null
+                || password == null
+                || interval == 0) {
+            throw new IllegalArgumentException("You set not all parameters in file rabbit.properties."
+                    + " Check your file.");
         }
     }
 
-    public int getSettingsFileProperties() {
+    private Connection initConnection() throws Exception {
+        Class.forName("org.postgresql.Driver");
+        this.connection = DriverManager.getConnection(url, login, password);
+        return this.connection;
+    }
+
+    public static void main(String[] args) throws Exception {
+        AlertRabbit alertRabbit = new AlertRabbit();
+        try (Connection connection = alertRabbit.initConnection()) {
+            alertRabbit.createTable(connection);
+            try {
+                Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+                scheduler.start();
+                JobDataMap data = new JobDataMap();
+                data.put("connection", connection);
+                JobDetail job = newJob(Rabbit.class)
+                        .usingJobData(data)
+                        .build();
+                SimpleScheduleBuilder times = simpleSchedule()
+                        .withIntervalInSeconds(alertRabbit.interval)
+                        .repeatForever();
+                Trigger trigger = newTrigger()
+                        .startNow()
+                        .withSchedule(times)
+                        .build();
+                scheduler.scheduleJob(job, trigger);
+                Thread.sleep(10000);
+                scheduler.shutdown();
+            } catch (Exception se) {
+                se.printStackTrace();
+            }
+        }
+    }
+
+    public void getSettingsFileProperties() {
         try (BufferedReader read = new BufferedReader(new FileReader(".\\src\\main\\resources\\rabbit.properties"))) {
             read.lines()
                     .filter(x -> x.length() != 0 && !x.startsWith("#"))
@@ -44,13 +82,60 @@ public class AlertRabbit {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return Integer.parseInt(values.get("rabbit.interval"));
+    }
+
+    public void createTable(Connection connection) throws Exception {
+            try (Statement statement = connection.createStatement()) {
+                String sql = String.format(
+                        "create table if not exists %s(%s, %s);",
+                        "rabbit",
+                        "created_date date",
+                        "created_time time"
+                );
+                statement.execute(sql);
+            }
+        }
+
+    @Override
+    public void close() throws Exception {
+        if (connection != null) {
+            connection.close();
+        }
     }
 
     public static class Rabbit implements Job {
+
+        public Rabbit() throws Exception {
+            SimpleDateFormat formatDate = new SimpleDateFormat("yyyy-MM-dd");
+            SimpleDateFormat formatTime = new SimpleDateFormat("HH:mm:ss");
+            Date date = new Date();
+            addJob("date '" + formatDate.format(date) + "'",
+                    "time '" + formatTime.format(date) + "'");
+        }
+
+        public void addJob(String date, String time)
+                throws Exception {
+            try (Connection connection = new AlertRabbit().initConnection()) {
+                try (Statement statement = connection.createStatement()) {
+                    String sql = String.format(
+                            "insert into rabbit(created_date, created_time) values (%s, %s);",
+                            date,
+                            time
+                    );
+                    statement.execute(sql);
+                }
+            }
+        }
+
         @Override
         public void execute(JobExecutionContext context) throws JobExecutionException {
-            System.out.println("Rabbit runs here ...");
+            try {
+                Connection connection = (Connection) context.getJobDetail().getJobDataMap().get("connection");
+                System.out.println("Rabbit runs here ...");
+                System.out.println("Write time in BD true");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 }
