5deb32162ea182cd479d3086974b98c23479247e
Just1lz <exelent.pride@rambler.ru>
2021-01-16 04:58:02 +0700

    3. Архитектура проекта - Аргегатора Java Вакансий [#409267]

---
+++ src/main/java/ru/job4j/db/SQLStore.java
@@ -0,0 +1,22 @@
+package ru.job4j.db;
+
+import ru.job4j.model.Post;
+
+import java.util.List;
+
+public class SQLStore implements Store{
+    @Override
+    public void save(Post post) {
+
+    }
+
+    @Override
+    public List<Post> getAll() {
+        return null;
+    }
+
+    @Override
+    public Post findById(String id) {
+        return null;
+    }
+}
